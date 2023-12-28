0ad3059ee5b8c761ba5eaef04ce7d015e6a83af8
Laziz Turakulov <4857092+LazaUK@users.noreply.github.com>
2023-12-28 19:37:54 +0000

    Add files via upload

--- GPT4V_Streamlit.py
+++ GPT4V_Streamlit.py
@@ -1,6 +1,5 @@
 # Importing required packages
 import streamlit as st
-from PIL import Image
 
 # Defining Web cam image details
 image_paths = {
@@ -19,14 +18,14 @@ if "camera" not in st.session_state:
 
 # Defining image analysis function
 def analyse_image(image):
-    result = "Test image analysis results"
+    result = "No out-of-stock items detected"
     return result
 
 # Creating sidebar with instructions
 st.sidebar.header("Instructions:")
 st.sidebar.markdown(
     """
-    This app allows you to choose between 4 Web cam images to select specific area of your fictitious shop.
+    This app allows you to choose between 4 Web cam images to check different areas of your fictitious shop.
     When you click specific Web cam button, its image is shown on the right side of the app.
     
     
@@ -47,16 +46,14 @@ result_placeholder = result_container.empty()
 for image_name, image_path in image_paths.items():
     # If the cam button is clicked, load the image and display it in the second column
     if col1.button(image_name):
-        image = Image.open(image_path)
-        image_placeholder.image(image=image, caption=image_name, use_column_width=True)
+        image_placeholder.image(image=image_path, caption=image_name, use_column_width=True)
         current_image = image_path
         current_image_name = image_name
         st.session_state.camera = image_name
         analyse_button = False
     # If the analysis button is clicked, preserve the last selected image
     elif st.session_state.camera == image_name:
-        image = Image.open(image_path)
-        image_placeholder.image(image=image, caption=image_name, use_column_width=True)
+        image_placeholder.image(image=image_path, caption=image_name, use_column_width=True)
         current_image = image_path
         current_image_name = image_name
         st.session_state.camera = image_name
@@ -67,7 +64,8 @@ if col1.button("Analyse"):
 
 # If the analysis button is clicked, use GPT-4V to analyse the image
 if analyse_button and current_image is not None:
+    st.snow() # New Year's theme :)
     result = analyse_image(current_image)
     result_placeholder.text(
-        f"Analysis results for {current_image_name}:\n{result}"
+        f"Image analysis results for {current_image_name}:\n{result}"
     )
